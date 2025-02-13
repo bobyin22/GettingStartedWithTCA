10ed754d1c02d1517765950373bf95c75ab7ba06
bobyin <bobyin22@gmail.com>
2025-02-13 12:15:04 +0800

    [modify] Essentials - Section 3 Testing network requests (測試會失敗) - 測試打 API 功能 - base video 2:25:28

--- GettingStartedWithTCA/GettingStartedWithTCATests/CounterFeatureTests.swift
+++ GettingStartedWithTCA/GettingStartedWithTCATests/CounterFeatureTests.swift
@@ -13,32 +13,6 @@ import Testing
 @MainActor
 struct CounterFeatureTests {
     @Test
-    func testTimer() async {
-        let store = TestStore(initialState: CounterFeature.State()) {
-            CounterFeature()
-        }
-        
-        await store.send(.toggleTimerButtonTapped) {
-            $0.isTimerRunning = true
-        }
-        
-        await store.receive(\.timerTick) {
-            $0.count = 1
-        }
-        
-        await store.receive(\.timerTick) {
-            $0.count = 2
-        }
-        
-        await store.receive(\.timerTick) {
-            $0.count = 3
-        }
-        
-        await store.send(.toggleTimerButtonTapped) {
-            $0.isTimerRunning = false
-        }
-    }
-    
 //    func basics() async {
 //        let store = TestStore(initialState: CounterFeature.State()) {
 //            CounterFeature()
@@ -51,4 +25,45 @@ struct CounterFeatureTests {
 //            $0.count = 0
 //        }
 //    }
+    
+//    func testTimer() async {
+//        let store = TestStore(initialState: CounterFeature.State()) {
+//            CounterFeature()
+//        }
+//        
+//        await store.send(.toggleTimerButtonTapped) {
+//            $0.isTimerRunning = true
+//        }
+//        
+//        await store.receive(\.timerTick) {
+//            $0.count = 1
+//        }
+//        
+//        await store.receive(\.timerTick) {
+//            $0.count = 2
+//        }
+//        
+//        await store.receive(\.timerTick) {
+//            $0.count = 3
+//        }
+//        
+//        await store.send(.toggleTimerButtonTapped) {
+//            $0.isTimerRunning = false
+//        }
+//    }
+    
+    func numberFact() async {
+      let store = TestStore(initialState: CounterFeature.State()) {
+        CounterFeature()
+      }
+        
+        await store.send(.factButttonTapped) {
+            $0.isLoading = true
+        }
+        
+        await store.receive(\.factResponse, timeout: .seconds(1)) {
+            $0.isLoading = false
+            $0.fact = "???"
+        }
+    }
 }
