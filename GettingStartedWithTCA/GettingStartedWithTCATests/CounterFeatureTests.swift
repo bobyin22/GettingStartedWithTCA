299c36b89c564d2bef2d9ef3780bbcdf26c33df2
bobyin <bobyin22@gmail.com>
2025-02-13 11:57:07 +0800

    [modify] Essentials - Section 2 Testing effects - 測試計時功能 - base video 2:20:33

--- GettingStartedWithTCA/GettingStartedWithTCATests/CounterFeatureTests.swift
+++ GettingStartedWithTCA/GettingStartedWithTCATests/CounterFeatureTests.swift
@@ -13,16 +13,42 @@ import Testing
 @MainActor
 struct CounterFeatureTests {
     @Test
-    func basics() async {
+    func testTimer() async {
         let store = TestStore(initialState: CounterFeature.State()) {
-              CounterFeature()
-            }
+            CounterFeature()
+        }
         
-        await store.send(.incrementButtonTapped) {
+        await store.send(.toggleTimerButtonTapped) {
+            $0.isTimerRunning = true
+        }
+        
+        await store.receive(\.timerTick) {
             $0.count = 1
         }
-        await store.send(.decrementButtonTapped) {
-            $0.count = 0
+        
+        await store.receive(\.timerTick) {
+            $0.count = 2
+        }
+        
+        await store.receive(\.timerTick) {
+            $0.count = 3
+        }
+        
+        await store.send(.toggleTimerButtonTapped) {
+            $0.isTimerRunning = false
         }
     }
+    
+//    func basics() async {
+//        let store = TestStore(initialState: CounterFeature.State()) {
+//            CounterFeature()
+//        }
+//        
+//        await store.send(.incrementButtonTapped) {
+//            $0.count = 1
+//        }
+//        await store.send(.decrementButtonTapped) {
+//            $0.count = 0
+//        }
+//    }
 }
